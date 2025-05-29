0008aafb0ba50867ecb179a343c0d40dd9033a22
Rahul Yadav <ry.rahul036@gmail.com>
2025-05-30 04:14:40 +0530

    fix

--- src/types/general.d.ts
+++ src/types/general.d.ts
@@ -7,4 +7,23 @@ export interface General {
   phoneNumber: string;
   phoneNumber2?: string;
   email: string;
+  footer: {
+    description: string;
+    navColumn1: {
+      links: {
+        text: string;
+        url: string;
+      }[];
+    };
+    navColumn2: {
+      links: {
+        text: string;
+        url: string;
+      }[];
+    };
+    emailPlaceholder: string;
+    signUpButtonText: string;
+    copyrightText: string;
+    backToTopText: string;
+  };
 }
