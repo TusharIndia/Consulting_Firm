5b920a44783189f28761acc1b045b325c33d498b
Pushpender Singh <singhpushpender250@gmail.com>
2025-05-30 19:56:40 +0530

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import { imageService } from "@unpic/astro/service";
+import sitemap from '@astrojs/sitemap';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://norislav.github.io",
+  site: "https://clearpath-25.netlify.app",
   base: "/",
   image: {
     service: imageService({
@@ -20,5 +21,12 @@ export default defineConfig({
     icon({
       iconDir: "src/assets/icons",
     }),
+    sitemap({
+      // Optional configuration
+      filter: (page) => !page.includes('/admin/'), // Exclude admin pages
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(), // Use actual last modified date if available
+    })
   ],
 });
